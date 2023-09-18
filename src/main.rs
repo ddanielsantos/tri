af0bdc1dce8fd72613742939abb31550100f811c
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2023-09-17 22:04:16 -0300

    fix: a lot of fixes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
 use std::{fs, io};
+use std::fs::ReadDir;
+use std::iter::Peekable;
+use std::path::PathBuf;
 
 trait Exitable {
     fn report_and_exit(&self);
@@ -69,8 +72,7 @@ impl From<io::Error> for ParseError {
 }
 
 #[derive(Debug)]
-enum ExecutionError {
-}
+enum ExecutionError {}
 
 impl Exitable for ExecutionError {
     fn report_and_exit(&self) {
@@ -107,60 +109,70 @@ fn parse_args(args: Vec<String>) -> Result<Args, ParseError> {
     }
 }
 
-fn exec_tri(args: Args, level: i32) -> Result<(), ExecutionError> {
+fn exec_tri(args: Args, level: usize) -> Result<(), ExecutionError> {
     let Args { path, dir_content } = args;
 
-    if level == 0  {
+    if level == 0 {
         println!("{}", path);
     }
 
     let mut peekable_dirs = dir_content.peekable();
 
     while let Some(entry) = peekable_dirs.next() {
-
         let path = &entry?.path();
 
-        if path.is_dir() {
-            let dir_content = fs::read_dir(path)?;
-            let args = Args {
-                path: path
-                    .file_name()
-                    .unwrap()
-                    .to_str()
-                    .unwrap()
-                    .to_string(),
-                dir_content,
-            };
+        if !has_next_entry(&mut peekable_dirs) {
+            print_last_entry(level, path);
 
-            exec_tri(args, level + 1)?;
-        }
-
-        if peekable_dirs.peek().is_some() {
-            println!("{}├── {}", get_identation(level), path
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap());
+            if path.is_dir() {
+                print_dir(level, path)?;
+            }
 
             continue;
         }
 
-        println!("{}└── {}", get_identation(level), path
+        println!("{}├── {}", get_indentation(level), path
             .file_name()
             .unwrap()
             .to_str()
-            .unwrap())
+            .unwrap());
+
+        if path.is_dir() {
+            print_dir(level, path)?;
+        }
     }
 
     Ok(())
 }
 
-fn get_identation(level: i32) -> String {
-    let mut identation = String::new();
+fn print_dir(level: usize, path: &PathBuf) -> Result<(), ExecutionError> {
+    let dir_content = fs::read_dir(path)?;
+    let args = Args {
+        path: path
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string(),
+        dir_content,
+    };
 
-    for _ in 0..level {
-        identation.push_str("│   ");
-    }
+    exec_tri(args, level + 1)?;
+    Ok(())
+}
 
-    identation
+fn has_next_entry(peekable_dirs: &mut Peekable<ReadDir>) -> bool {
+    peekable_dirs.peek().is_some()
+}
+
+fn print_last_entry(level: usize, path: &PathBuf) {
+    println!("{}└── {}", get_indentation(level), path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap());
+}
+
+fn get_indentation(level: usize) -> String {
+    "    ".repeat(level)
 }
